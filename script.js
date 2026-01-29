035cf1d25c464d728c745d77fb0fb0badff32a52
Nirmal <nirmalgoyal0108@gmail.com>
2026-01-29 12:38:39 +0530

    feat: display userInfo and repoInfo

--- script.js
+++ script.js
@@ -3,23 +3,56 @@ const searchBtn = document.getElementById("search");
 
 searchBtn.addEventListener("click", handleSearch);
 
-function handleSearch(){
+async function handleSearch(){
     const username = usernameInput.value.trim();
     if(!username){
         alert("Please enter valid a username");
         return;
     }
-    fetch(`https://api.github.com/users/${username}`)
-    .then(response => {
-        if(!response.ok){
-            throw new Error("User not found")
+    
+    try{
+        const userResponse = await fetch(`https://api.github.com/users/${username}`);
+        if(!userResponse.ok){
+            throw new Error("User not found");
         }
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-    })
-    .catch(error => {
-        console.error(error.message)
-    })
+
+        const userData = await userResponse.json();
+        displayUserInfo(userData);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=5`);
+        if(!repoResponse.ok){
+            throw new Error("Could not fetch repositories");
+        }
+
+        const repoData = await repoResponse.json();
+        displayUserRepos(repoData);
+    } catch(error){
+        alert(error.message);
+    }
+}
+
+function displayUserInfo(user){
+    const userInfoSection = document.getElementById('user-info');
+    userInfoSection.innerHTML = `
+        <h2>${user.name}</h2>
+        <img src="${user.avatar_url}">
+        <p>Bio: ${user.bio}</p>
+        <p>Followers: ${user.followers} / Following: ${user.following}</p>
+        <p>Public Repositories count: ${user.public_repos}</p>
+        `;
+}
+
+function displayUserRepos(repos){
+    const userRepoSection = document.getElementById('user-repos');
+    userRepoSection.innerHTML = `
+        <h3>Latest 5 Repositories:</h3>
+        <ul>
+            ${repos.map(repo => `
+                <li>
+                    <h4>${repo.name}</h4>
+                    <p><a href="${repo.html_url}">View repositort</a></p>
+                </li>
+            `).join("")}
+        </ul>
+    `;
 }
